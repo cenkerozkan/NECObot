9bae5fac191a27be07917e0ac0b114e5ef66fe54
cenkerozkan <ozkan.cenker@student.atilim.edu.tr>
2024-12-04 14:39:09 +0300

    CREATE MESSAGE MDOEL

---
+++ BLL/Models/MessageModel.cs
@@ -0,0 +1,15 @@
+#nullable disable
+
+using BLL.DAL;
+using System.ComponentModel;
+using System.Collections.Generic;
+
+namespace BLL.Models
+{
+    public class MessageModel
+    {
+        public Message Record { get; set; }
+        public Message Message { get; set; }
+    }
+}
+
